0780c087035747f4769144d97232386e105cd651
Cameron Zorio <cameronzorio@gmail.com>
2025-06-02 00:24:32 -0700

    finished agent based algs

---
+++ agent_based_algs/.ipynb_checkpoints/flock-checkpoint.py
@@ -0,0 +1,12 @@
+from bird import Bird
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Flock():
+    def __init__(self,size,a):
+        birds = []
+        for i in range(size):
+            birds.append(Bird(a))
+
+    def plot():
+        pass
